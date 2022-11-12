ed9d9a66944b83c0a66676aaaf71f179d21fa2f8
Musiker15 <info@musiker15.de>
2022-11-12 23:18:49 +0100

    Release

--- server.lua
+++ server.lua
@@ -28,13 +28,13 @@ local CurrentVersion = GetCurrentVersion()
 local resourceName = "^4["..GetCurrentResourceName().."]^0"
 
 if Config.VersionChecker then
-	PerformHttpRequest('https://raw.githubusercontent.com/Musiker15/VERSIONS/main/VERSION_WEAPONAMMOITEMS', function(Error, NewestVersion, Header)
+	PerformHttpRequest('https://raw.githubusercontent.com/MSK-Scripts/msk_weaponammoitems/main/VERSION', function(Error, NewestVersion, Header)
 		print("###############################")
     	if CurrentVersion == NewestVersion then
 	    	print(resourceName .. '^2 ✓ Resource is Up to Date^0 - ^5Current Version: ^2' .. CurrentVersion .. '^0')
     	elseif CurrentVersion ~= NewestVersion then
         	print(resourceName .. '^1 ✗ Resource Outdated. Please Update!^0 - ^5Current Version: ^1' .. CurrentVersion .. '^0')
-	    	print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here: ^9https://keymaster.fivem.net/^0')
+	    	print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here: ^9https://github.com/MSK-Scripts/msk_weaponammoitems/releases/tag/v'.. NewestVersion .. '^0')
     	end
 		print("###############################")
 	end)
